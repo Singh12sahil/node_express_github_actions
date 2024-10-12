0d89d1ddb992ad31a96f27194e72829fa680d155
Singh12sahil <sukhig914@gmail.com>
2024-10-12 10:25:14 -0400

    "update name"

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
     res.send("<h1>Home Page</h1>")
 })
 
-//http://localhost:3000/
+//http://localhost:3000/name
 app.get('/name', function (req, res) {
   res.send("<h1>C0907196 ---- Sukhveer Singh/h1>")
 })
